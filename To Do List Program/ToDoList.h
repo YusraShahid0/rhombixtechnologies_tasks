35c5b5fc69ef0345b28e77ff8c1b837e6232c853
Yusra Shahid <ys09223@st.habib.edu.pk>
2025-07-08 09:32:40 +0500

    Update ToDoList.h

--- To Do List Program/ToDoList.h
+++ To Do List Program/ToDoList.h
@@ -7,16 +7,10 @@
 
 using namespace std;
 
-//Helper Function Declarations
-bool list_is_empty(const vector<string> &todo_list);
-void trim_input(string &input);
-bool valid_input(const string &input);
-
-//Principal Function Declarations
 int menu();
 void add_task(vector<string> &todo_list);
 void mark_as_complete(vector<string> &todo_list);
 void view_tasks(vector<string> &todo_list);
-
+bool list_is_empty(const vector<string> &todo_list);
 
 #endif 
