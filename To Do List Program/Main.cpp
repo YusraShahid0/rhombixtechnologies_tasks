7faaab1170cc8859d6b5c8d0ef3b9e915da722ef
Yusra Shahid <ys09223@st.habib.edu.pk>
2025-07-08 09:35:17 +0500

    Update Main.cpp

--- To Do List Program/Main.cpp
+++ To Do List Program/Main.cpp
@@ -8,40 +8,30 @@ using namespace std;
 
 int main()
 {
-    vector<string> todo_list;
-    cout << "Helloo! It's sad to see you're back to your to-do list. But no worries it's better to put the task on the list and feel a step closer to completing it rather than actually doing it... hehe" << endl;
-    int choice;
+    //initializing the todolist in the form of a vector of strings
+    vector<string> todolist;
     
-    while (true)
-    {
+    //initializing the variable for the user's choice
+    int choice;
+
+    //running the program continuously
+    while (true){
+        //getting the choice of the user
         choice = menu();
 
-         if (cin.fail()) {
-            cin.clear(); // clear the fail state
-            cin.ignore(10000, '\n'); // discard bad input
-            cout << "Invalid input. Please enter a number.\n";
-            continue; // go back to the menu
-         }
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Please enter a valid input (number)" << endl;
+            continue;
+        }
 
-        switch (choice)
-        {
-        case 1:
-            cout << endl << "Ahhh! this is where procrastination begins..." << endl;
-            add_task(todo_list);
-            break;
-
-        case 2:
-            cout << endl << "wow gurlll! u actually got something completed!" << endl;
-            mark_as_complete(todo_list);
-            break;
-
-        case 3:
-            cout << endl << "haha... Welcome to Your Journey to Depression Land." << endl;
-            view_tasks(todo_list);
-            break;
-        case 4:
-            return 0;
-            break;
+        //calling the functions according to the user's choice
+        switch(choice) {
+            case(1) : add_tasks(todolist); break;
+            case(2) : mark_tasks(todolist); break;
+            case(3) : view_tasks(todolist); break;
+            case(4) : return 0;
         }
     }
 }
